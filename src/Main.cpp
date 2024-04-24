8c84f245d61682d5ced6507a06dfb6cc248f18a3
George Fotopoulos <xorz57@gmail.com>
2024-04-24 20:56:15 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,110 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <cinttypes>
+#include <cstdlib>
+#include <iostream>
+
 int main() {
+    if (!glfwInit()) {
+        std::exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Graphite", nullptr, nullptr);
+    if (window == nullptr) {
+        glfwTerminate();
+        std::exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        std::exit(EXIT_FAILURE);
+    }
+
+    std::clog << "OpenGL Vendor: " << (const char *) glGetString(GL_VENDOR) << std::endl;
+    std::clog << "OpenGL Renderer: " << (const char *) glGetString(GL_RENDERER) << std::endl;
+    std::clog << "OpenGL Version: " << (const char *) glGetString(GL_VERSION) << std::endl;
+    std::clog << "OpenGL Shading Language Version: " << (const char *) glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    std::uint64_t frame = 0;
+    double t0 = glfwGetTime();
+
+    while (!glfwWindowShouldClose(window)) {
+        ++frame;
+        const double t1 = glfwGetTime();
+        const double deltaTime = t1 - t0;
+        t0 = t1;
+
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Graphite");
+        ImGui::Text("frame: %" PRIu64, frame);
+        ImGui::Text("deltaTime: %.5lf sec", deltaTime);
+        ImGui::End();
+
+        ImGui::Render();
+
+        int display_w;
+        int display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            GLFWwindow *backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
