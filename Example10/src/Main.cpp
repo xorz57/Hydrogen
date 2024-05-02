c67bd41e1015750e4a110a7d5d66a7657642d43e
George Fotopoulos <xorz57@gmail.com>
2024-05-02 23:45:07 +0300

    Rename variables

--- Example10/src/Main.cpp
+++ Example10/src/Main.cpp
@@ -118,11 +118,11 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    int width, height;
+    int texture_w, texture_h;
     stbi_set_flip_vertically_on_load(true);
-    stbi_uc *pixels = stbi_load("assets/textures/texture.png", &width, &height, nullptr, STBI_rgb_alpha);
+    stbi_uc *pixels = stbi_load("assets/textures/texture.png", &texture_w, &texture_h, nullptr, STBI_rgb_alpha);
     if (pixels) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void *) pixels);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture_w, texture_h, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void *) pixels);
         glGenerateMipmap(GL_TEXTURE_2D);
         stbi_image_free(pixels);
     }
