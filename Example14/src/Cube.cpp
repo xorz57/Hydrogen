4a14107681df5d98f152c499dfac01bf6474bc95
George Fotopoulos <xorz57@gmail.com>
2024-05-03 15:14:51 +0300

    Fix Cube

--- Example14/src/Cube.cpp
+++ Example14/src/Cube.cpp
@@ -1,18 +1,21 @@
 #include "Cube.hpp"
 
 Cube::Cube() {
+    mVAO.Bind();
     mVBO.Bind();
     mVAO.SetFloat3(0, sizeof(Vertex), (void *) offsetof(Vertex, position));
     mVAO.SetFloat2(1, sizeof(Vertex), (void *) offsetof(Vertex, textureCoordinates));
     mVBO.Unbind();
+    mVAO.Unbind();
 }
 
 void Cube::Draw(Shader &shader) {
     shader.SetFloat4x4("u_Model", mModel);
-
     mModel = glm::mat4(1.0f);
 
+    mVAO.Bind();
     glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(mElements.size()), GL_UNSIGNED_INT, (void *) nullptr);
+    mVAO.Unbind();
 }
 
 void Cube::Delete() {
