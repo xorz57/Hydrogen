6b7958b0645cfe720ff3504198af9123d4ccbadf
George Fotopoulos <xorz57@gmail.com>
2024-05-07 23:26:03 +0300

    Update Camera

--- Example16/src/Camera.hpp
+++ Example16/src/Camera.hpp
@@ -16,10 +16,10 @@ public:
     glm::mat4 GetProjection(float aspect) const;
 
 private:
-    glm::vec3 mPosition{+0.0f, +0.0f, +0.0f};
     glm::vec3 mFront{+0.0f, +0.0f, -1.0f};
     glm::vec3 mUp{+0.0f, +1.0f, +0.0f};
     glm::vec3 mRight = glm::normalize(glm::cross(mFront, mUp));
+    glm::vec3 mPosition{+0.0f, +0.0f, +0.0f};
     float mFOV = 45.0f;
     float mNear = 0.1f;
     float mFar = 100.0f;
