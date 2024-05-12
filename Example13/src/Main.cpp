1ff7dc8c09e63bf9f9b1ebd71258167c16dee87e
George Fotopoulos <xorz57@gmail.com>
2024-05-12 18:27:20 +0300

    Update Main.cpp

--- Example13/src/Main.cpp
+++ Example13/src/Main.cpp
@@ -197,7 +197,7 @@ int main() {
         auto model = glm::mat4(1.0f);
         model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(-55.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(-45.0f), glm::vec3(1.0f, 1.0f, 0.0f));
         const glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
         const glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(window_w) / static_cast<float>(window_h), 0.1f, 100.0f);
 
