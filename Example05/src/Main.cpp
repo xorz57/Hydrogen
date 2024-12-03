66b1273114299331996e2e5a376c6433170c8f8a
George Fotopoulos <xorz57@gmail.com>
2024-12-03 02:51:17 +0200

    Reorder instructions

--- Example05/src/Main.cpp
+++ Example05/src/Main.cpp
@@ -107,8 +107,8 @@ layout (location = 1) in vec3 a_color;
 out vec3 v_color;
 
 void main() {
-    gl_Position = vec4(a_position, 1.0);
     v_color = a_color;
+    gl_Position = vec4(a_position, 1.0);
 }
         )";
 
--- Example10/assets/shaders/default_vert.glsl
+++ Example10/assets/shaders/default_vert.glsl
@@ -6,6 +6,6 @@ layout (location = 1) in vec2 a_texture_coordinates;
 out vec2 v_texture_coordinates;
 
 void main() {
-    gl_Position = vec4(a_position, 1.0);
     v_texture_coordinates = a_texture_coordinates;
+    gl_Position = vec4(a_position, 1.0);
 }
