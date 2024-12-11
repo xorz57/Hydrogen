3a3301974a07fa1a4ed492ea00d4d63d04161f1c
George Fotopoulos <xorz57@gmail.com>
2024-12-12 01:26:13 +0200

    Use GL_TRIANGLES instead

--- Example16/src/Shapes/Circle.cpp
+++ Example16/src/Shapes/Circle.cpp
@@ -34,21 +34,26 @@ void Circle::Build(const std::uint32_t sectors) {
     const float sector_step = 2.0f * glm::pi<float>() / static_cast<float>(sectors);
 
     mVertices.push_back({glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)});
-    mElements.push_back(0);
 
-    for (std::uint32_t sector = 0; sector <= sectors; ++sector) {
+    for (std::uint32_t sector = 0; sector < sectors; ++sector) {
         const float sector_angle = static_cast<float>(sector) * sector_step;
         const float x = 0.5f * glm::cos(sector_angle);
         const float y = 0.5f * glm::sin(sector_angle);
         mVertices.push_back({glm::vec3(x, y, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f)});
-        mElements.push_back(sector + 1);
+
+        GLuint current = sector + 1;
+        GLuint next = (sector + 1) % sectors + 1;
+
+        mElements.push_back(0);
+        mElements.push_back(current);
+        mElements.push_back(next);
     }
 }
 
 void Circle::Draw() const {
     mVAO->Bind();
 
-    glDrawElements(GL_TRIANGLE_FAN, static_cast<GLsizei>(mElements.size()), GL_UNSIGNED_INT, static_cast<void *>(nullptr));
+    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(mElements.size()), GL_UNSIGNED_INT, static_cast<void *>(nullptr));
 
     VAO::Unbind();
 }
